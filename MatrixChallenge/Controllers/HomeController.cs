39ce1160672bbbbb8ad1d1f600c113bbf613acc8
User <User@MICHELLEW2617>
2017-06-14 15:35:50 -0500

    Reviewing further

---
+++ MatrixChallenge/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls.WebParts;
+using MatrixChallenge.Models;
+
+namespace MatrixChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Matrix model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+
+
+            }
+            return View("Index", model);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MatrixChallenge/Models/Matrix.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace MatrixChallenge.Models
+{
+    public class Matrix
+    {
+        
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+
+        public Matrix(int r, int c)
+        {
+            this.Rows = r;
+            this.Columns = c;
+        }
+
+        public Matrix Add(Matrix matrix2)
+        {
+            
+            var sum = new Matrix(matrix2.Rows, matrix2.Columns);
+            return sum;
+        }
+    }
+}
